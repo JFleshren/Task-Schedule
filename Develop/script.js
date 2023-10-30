60376767c030082056e948532c47c59c0a865102
Fleshren <fleshren1@gmail.com>
2023-10-30 18:36:31 -0500

    broken time

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   var timeBlockContainer = $("#time-block-container");
   var currentHour = dayjs().hour();
+  var savedText = JSON.parse(localStorage.getItem("savedText")) || {}
   for (var hour = 9; hour <= 17; hour++) {
       var formattedHour = dayjs({ hour: hour }).format("hA");
       var currentHour = dayjs().hour();
       var timeBlock = $("<div>")
           .attr("id", "hour-" + hour)
           .addClass("row time-block");
+          
 
       if (hour < currentHour) {
           timeBlock.addClass("past");
@@ -22,7 +24,16 @@ $(document).ready(function() {
           <i class="fas fa-save" aria-hidden="true"></i>
       </button>
   `);
-
+  (function(hourId) {
+    var saveButton = timeBlock.find(".saveBtn");
+    saveButton.click(function() {
+        var textArea = $(this).siblings(".description");
+        var enteredText = textArea.val().trim();
+        savedText[hourId] = enteredText;
+        localStorage.setItem("savedText", JSON.stringify(savedText));
+        alert("Event Added!");
+    });
+})("hour-" + hour);
   timeBlockContainer.append(timeBlock);
     }
 });
